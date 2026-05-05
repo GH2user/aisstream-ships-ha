d14d4f24e33ef2eb85d81a6fea2df865c125088e
GH2user <59734822+GH2user@users.noreply.github.com>
2026-05-05 12:17:45 +0200

    Update vessel types and status map in const.py
    Added more status options, allow all vessel types

--- custom_components/aisstream_ships/const.py
+++ custom_components/aisstream_ships/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "aisstream_ships"
 AISSTREAM_WS = "wss://stream.aisstream.io/v0/stream"
-PASSENGER_TYPES = set(range(60, 70))
+VESSEL_TYPES = set(range(10, 99))
 
 CONF_API_KEY = "api_key"
 CONF_MAX_SHIPS = "max_ships"
@@ -12,8 +12,9 @@ DEFAULT_MIN_LENGTH = 0
 DEFAULT_BBOX = [[[53.25, -3.20], [53.50, -2.85]]]
 
 STATUS_MAP = {
-    0: "Underway", 1: "Anchored", 3: "Restricted",
-    5: "Moored", 8: "Sailing", -1: "Unknown"
+    0: "Underway", 1: "Anchored", 2: "Not Under Command", 3: "Restricted",
+    4: "Constrained by Draught", 5: "Moored", 6: "Aground", 7: "Fishing",
+    8: "Sailing", -1: "Unknown"
 }
 
 SIGNAL_UPDATE = f"{DOMAIN}_update"
